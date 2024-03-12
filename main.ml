349aa69f5813be8bdfbdbbef5257d89f945874a7
Anup-Nayak <gpnayak1930@gmail.com>
2024-03-12 19:40:23 +0530

    1,2,3 done and tested

--- main.ml
+++ main.ml
@@ -1,5 +1,6 @@
-type symbol = (string * int);;
+open List;;
 
+type symbol = (string * int);;
 type signature = symbol list;;
 
 type tree = V of string | C of { node: symbol ; children: tree list };;
@@ -56,8 +57,74 @@ print_endline(string_of_bool(check_sig test3));;
 print_endline(string_of_bool(check_sig test4));;
 print_endline(string_of_bool(check_sig test5));;
 print_endline(string_of_bool(check_sig test6));;
+
 *)
 
+let rec wftree (sign : signature) (t : tree) : bool =
+  let rec check_node ((s, arity) : symbol) (children : tree list) : bool =
+    let rec check_children_validity (children : tree list) : bool =
+      List.for_all (wftree sign) children
+    in
+    let expected_arity = List.length children in
+    arity = expected_arity && check_children_validity children
+  in
+  match t with
+  | V _ -> true 
+  | C { node; children } -> check_node node children
 
+
+
+let sig1 = [ ("0", 0); ("1", 0); ("+", 2); ("*", 2) ];;
+
+let x = V "x";;
+let y = V "y";;
+let z = V "z";;
+
+let zero = C {node = ("0", 0); children = []};;
+let one = C {node = ("1", 0); children = []};;
+let plus_zero_one = C {node = ("+", 2); children = [zero] };;
+let times_one_x = C {node = ("*", 2); children = [one; x] };;
+let plus_zero_y = C {node = ("+", 2); children = [zero; y] };;
+let plus_timesonex_pluszeroy = C {node = ("+", 2) ; children = [times_one_x; plus_zero_y ] };;
+let plus_timesonex_z = C {node = ("+", 2); children = [times_one_x; z ] };;
+
+
+
+let rec ht t = match t with
+    | V _ -> 0
+    | C r ->
+        let (s,n) = r.node
+        in
+        if n = 0 then
+            0
+        else 
+            1+(fold_left max 0 (map ht r.children));;
+
+
+let rec size t = match t with
+    | V _ -> 1
+    | C r -> 1+(fold_left (+) 0 (map size r.children) );;
+
+
+(*print_endline(string_of_int(size plus_timesonex_pluszeroy));;*)
+
+type variable = string
+
+let rec vars (t : tree) : variable list =
+  let rec collect_vars acc = function
+    | V v -> v :: acc
+    | C { node = _; children } ->
+        List.fold_left (fun acc child -> collect_vars acc child) acc children
+  in
+  collect_vars [] t
+
+(*
+let vars_set = vars times_one_x
+
+
+let () =
+  print_endline "Variables in the tree:";
+  List.iter (fun var -> print_endline var) vars_set
+*)
 
 
